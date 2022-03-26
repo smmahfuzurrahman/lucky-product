ae14dab73b93cfddb893a258ddd81410c89a908f
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-03-26 23:27:42 +0600

    answer part added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
-
+import'./Footer.css'
 const Footer = () => {
     return (
-        <div>
-            <h3>How React Work</h3>
-            <p>Ans: React is a free oper java</p>
-
-
-            <h3> How useState work</h3>
-            <p>Ans: </p>
+        <div className='answer'>
+            <h3>Question 01: How React Work</h3>
+            <p><b>Ans:</b> React is a free open source java script Library. This creates a fake HTML document. So that HTML tags can be written easily. Everyone has the advantage of working on a separate file. React A can easily write HTML as well as CSS work can be done easily.</p>
+            <h3>Question 02: How useState work</h3>
+            <p><b>Ans:</b> useStare is a type of hook. Where the necessary elements are allowed to remain in a variable called a state. It contains a variable by which it updates the value of the variable and displays another function.When fetching data from the API, useState is followed by another state called useEffect.</p>
         </div>
     );
 };
